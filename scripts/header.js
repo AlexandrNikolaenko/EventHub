ebbc448d260bd1d8425cc61d99cfa8ad22695c1f
Александр <nikol.alex06@mail.ru>
2025-10-03 17:37:32 +0300

    много всего

---
+++ scripts/header.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById('menu-bar');
+const menuList = document.getElementById('header__menu__list');
+
+function handleChangeVisible() {
+  menuList.classList.toggle('enable');
+  menuList.classList.toggle('disable');
+}
+
+menuButton.addEventListener('click', handleChangeVisible);
