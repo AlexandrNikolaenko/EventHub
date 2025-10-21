69eba13792f30e3fc430642b1e571203d1a180e3
Александр <nikol.alex06@mail.ru>
2025-10-22 01:33:58 +0300

    fix: fix nav bar

--- scripts/header.js
+++ scripts/header.js
@@ -3,6 +3,7 @@ import { logout, user } from "./api.js";
 // подсвечивание активной страницы
 
 const links = document.getElementsByClassName('nav_link');
+const navBarLinks = document.getElementsByClassName('nav_bar__link');
 const page = document.location.pathname;
 
 Array.from(links).forEach(link => {
@@ -12,7 +13,16 @@ Array.from(links).forEach(link => {
   ) {
     link.classList.add('active');
   }
-})
+});
+
+Array.from(navBarLinks).forEach(link => {
+  if (((page == '/' || page == '/index.html') && link.getAttribute('href') == './index.html') || 
+  (page == '/main.html' && link.getAttribute('href') == './main.html') ||
+  (page == '/about.html' && link.getAttribute('href') == './about.html')
+  ) {
+    link.classList.add('active');
+  }
+});
 
 // вход в аккаунт и выход из аккаунта пользователя
 
