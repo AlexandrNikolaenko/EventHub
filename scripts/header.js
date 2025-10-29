b6f1a4d81172011ba31140b44fc1ba11365e26f5
Александр <nikol.alex06@mail.ru>
2025-10-29 08:54:18 +0300

    fix: fix links for gh

--- scripts/header.js
+++ scripts/header.js
@@ -7,18 +7,18 @@ const navBarLinks = document.getElementsByClassName('nav_bar__link');
 const page = document.location.pathname;
 
 Array.from(links).forEach(link => {
-  if (((page == '/' || page == '/index') && link.getAttribute('href') == '/EventHub/') || 
-  (page == '/main/' && link.getAttribute('href') == '/EventHub/main') ||
-  (page == '/about/' && link.getAttribute('href') == '/EventHub/about')
+  if (((page == '/EventHub/' || page == '/EventHub/') && link.getAttribute('href') == '/EventHub/') || 
+  (page == '/EventHub/main/' && link.getAttribute('href') == '/EventHub/main') ||
+  (page == '/EventHub/about/' && link.getAttribute('href') == '/EventHub/about')
   ) {
     link.classList.add('active');
   }
 });
 
 Array.from(navBarLinks).forEach(link => {
-  if (((page == '/' || page == '/EventHub/') && link.getAttribute('href') == '/EventHub/') || 
-  (page == '/main/' && link.getAttribute('href') == '/EventHub/main') ||
-  (page == '/abou/' && link.getAttribute('href') == '/EventHub/about')
+  if (((page == '/EventHub/' || page == '/EventHub/') && link.getAttribute('href') == '/EventHub/') || 
+  (page == '/EventHub/main/' && link.getAttribute('href') == '/EventHub/main') ||
+  (page == '/EventHub/about/' && link.getAttribute('href') == '/EventHub/about')
   ) {
     link.classList.add('active');
   }
